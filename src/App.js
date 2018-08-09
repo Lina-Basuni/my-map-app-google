8d0407923df1cc11397185a43bb4e378ee6c79f8
Lina Ahmed Kamal Basuni <lina.ahmedkamal@gmail.com>
2018-08-09 06:55:27 +0200

    create menu

--- src/App.js
+++ src/App.js
@@ -2,26 +2,49 @@ import React, { Component } from 'react';
 import { GoogleMap, Marker } from "react-google-maps";
 import Map from './map';
 import logo from './logo.svg';
+import Menu from './menu';
 import './App.css';
 
 class App extends Component {
+
+  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+  openNav= ()=> {
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+  }
+
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  closeNav= ()=> {
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("main").style.marginLeft = "0";
+  }
+
   render() {
     return (
+
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">My Map</h1>
-        </header>
-        <body>
-          <div className="mapContainer">
-            <Map
-              googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-              loadingElement={<div style={{ height: `100%` }} />}
-              containerElement={<div style={{ height: `590px` }} />}
-              mapElement={<div style={{ height: `100%` }}/>}
-            />
+        <div id="main">
+          <header className="App-header">
+          <div className="menu_container">
+          <Menu
+            openNav={this.openNav}
+            closeNav={this.closeNav}
+          />
           </div>
-        </body>
-      </div>
+          <h1 className="App-title">My Map</h1>
+          </header>
+          <body>
+          <div className="mapContainer">
+          <Map
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `590px` }} />}
+          mapElement={<div style={{ height: `100%` }}/>}
+          />
+          </div>
+          </body>
+          </div>
+        </div>
     );
   }
 }
---
+++ src/menu.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Menu extends Component{
+  render(){
+    return(
+      <div className="menu">
+        <div className="menu_icon"
+        onClick={
+            ()=>{
+              this.props.openNav()
+            }
+          }>
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
+        </div>
+        <div id="mySidenav" className="sidenav">
+          <a href="javascript:void(0)" className="closebtn" onClick={()=>{
+            this.props.closeNav()
+          }}>&times;</a>
+          <a></a>
+          <a></a>
+          <a></a>
+          <a></a>
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default Menu;
